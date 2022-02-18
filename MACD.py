13735b20640b50db0ade77dc9d2a875c61743b74
LAWTED <3209571656@qq.com>
2022-02-18 21:35:44 +0800

    fix:MACD

--- MACD.py
+++ MACD.py
@@ -31,14 +31,21 @@ long_win = 26    # 长期EMA平滑天数
 macd_win = 20     # DEA线平滑天数
 
 # data 日期 close 收盘价
-# buy = []
-# sell = []
+buy = []
+sell = []
 # fw = open("./query_deal.txt", 'w')
 
+df2 = {
+    'date': [],
+    'close': []
+}
+for k, v in result.items():
+    df2['close'].append(float(v))
+    df2['date'].append(k)
 
-def MACD(df2, fast, slow, signal, buy, sell):
+def MACD(df2):
     dif, dea, hist = ta.MACD(
-        np.array(df2['close']), fastperiod=fast, slowperiod=slow, signalperiod=signal)
+        np.array(df2['close']), fastperiod=12, slowperiod=26, signalperiod=9)
     df3 = pd.DataFrame({'dif': dif[33:], 'dea': dea[33:], 'hist': hist[33:]},
                        index=df2['date'][33:], columns=['dif', 'dea', 'hist'])
     # df3.plot(title='MACD')
@@ -58,12 +65,10 @@ def MACD(df2, fast, slow, signal, buy, sell):
             # if df3.index[i+1] == df2['date'][-1]:
                 # print('sell ' + df3.index[i+1])
     # print(buy, sell)
-    total = calWealth(buy, sell)
-    # return(dif, dea, hist, buy, sell)
-    return total
+    return(dif, dea, hist)
 
 # df = []
-# 生成每日历史数据
+# # 生成每日历史数据
 # for i in range(1,len(result)):
 #     df.append(dict(itertools.islice(result.items(), i)))
 
@@ -78,7 +83,7 @@ def MACD(df2, fast, slow, signal, buy, sell):
 
 #     MACD(df2)
 
-
+MACD(df2)
 buyTime = []
 sellTime = []
 earn = []
@@ -88,33 +93,30 @@ wealthArray = []
 def calWealth(buy, sell):
     wealth = 1000
     hold = 0
-    # print(len(buy))
-    # print(len(sell))
+
     firstBuyDate = datetime.strptime(buy[0], '%m/%d/%y')
     firstSellDate = datetime.strptime(sell[0], '%m/%d/%y')
-    # print(buy, sell)
     # 第一天就买入了
     if firstBuyDate > firstSellDate:
         buy = ['9/11/16'] + buy
-
-        # buy 98个日期 sell 98个日期
     for i in range(len(buy)):
-        # 当前的钱除价格算 份额
         # 只有涨了才卖
         if result[buy[i]] < result[sell[i]]:
-            buyTime.append(datetime.strptime(buy[i], '%m/%d/%y'))  # 添加买入时间
-            sellTime.append(datetime.strptime(sell[i], '%m/%d/%y'))  # 卖出
             hold = wealth / result[buy[i]]
-            # wealth 增加了 卖出价格 减去 买入价格 乘份额
-            wealth += hold * (result[sell[i]] - result[buy[i]])
-            earn.append(hold * (result[sell[i]] - result[buy[i]]))
-            wealthArray.append(wealth)
-            # print('buy at '+buy[i]+' sell at ' + sell[i] + ' earn ' + str(
-                # hold * (result[sell[i]] - result[buy[i]])) + ' NOW Wealth ' + str(wealth))
-
+            # 收益除成本大于手续费 比特币手续费2%
+            if ((result[sell[i]] - result[buy[i]]) * hold) / wealth > 0.02:
+                buyTime.append(datetime.strptime(buy[i], '%m/%d/%y'))  # 添加买入时间
+                sellTime.append(datetime.strptime(sell[i], '%m/%d/%y'))  # 卖出
+                # wealth 增加了 卖出价格 减去 买入价格 乘份额
+                wealth += hold * (result[sell[i]] - result[buy[i]]) * 0.98
+                earn.append(hold * (result[sell[i]] - result[buy[i]]))
+                wealthArray.append(wealth)
+                # print('buy at '+buy[i]+' sell at ' + sell[i] + ' earn ' + str(
+                    # hold * (result[sell[i]] - result[buy[i]])) + ' NOW Wealth ' + str(wealth))
+    # print(len(buyTime))
     return wealth
-# total = calWealth(buy,sell)
-# print(total)
+total = calWealth(buy,sell)
+print(total)
 
 
 def write2cvs(buyTime, sellTime, earn, wealthArray):
@@ -125,36 +127,30 @@ def write2cvs(buyTime, sellTime, earn, wealthArray):
 # write2cvs(buyTime, sellTime, earn, wealthArray)
 
 
-df2 = {
-    'date': [],
-    'close': []
-}
-for k, v in result.items():
-    df2['close'].append(float(v))
-    df2['date'].append(k)
 
 
-def findBest():
 
-    ans = 0
-    buy = []
-    sell = []
-    i = 6
-    j = 30
-    k = 6
-    res = []
-    for i in range(2, 30):
-        for j in range(10, 100):
-            for k in range(2, 30):
-                total = MACD(df2, i, j, k, buy, sell)
-                buy = []
-                sell = []
-                f = open("a.txt", 'a')
-                f.write("\n %d %d %d %d" %(i, j, k, total) )
-                f.close()
-                ans = max(ans,total)
-                if ans == total:
-                    res = [i,j,k]
-    print(res,ans)
+# def findBest():
 
-findBest()
+#     ans = 0
+#     buy = []
+#     sell = []
+#     i = 6
+#     j = 30
+#     k = 6
+#     res = []
+#     for i in range(2, 30):
+#         for j in range(10, 100):
+#             for k in range(2, 30):
+#                 total = MACD(df2, i, j, k, buy, sell)
+#                 buy = []
+#                 sell = []
+#                 f = open("a.txt", 'a')
+#                 f.write("\n %d %d %d %d" %(i, j, k, total) )
+#                 f.close()
+#                 ans = max(ans,total)
+#                 if ans == total:
+#                     res = [i,j,k]
+#     print(res,ans)
+
+# findBest()
